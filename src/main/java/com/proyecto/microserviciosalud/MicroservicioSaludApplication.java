0ea87259177732b021e0f05c2be8b5b2c03af6eb
Samuu10 <svallmun@myuax.com>
2024-12-10 18:20:43 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microserviciosalud/MicroservicioSaludApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciosalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioSaludApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioSaludApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microserviciosalud/MicroservicioSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciosalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSaludApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
